27b819475762fe6f38dc655fd8ad1b770b141dd8
pedrofabian455 <pedrofabianzarate@gmail.com>
2023-05-17 23:14:50 -0400

    clase 11

---
+++ src/main/java/com/pedro/ContactoServlet.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pedro;
+
+import com.pedro.dao.ContactoDAO;
+import com.pedro.dao.ContactoDAOImpl;
+import com.pedro.model.Contacto;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(urlPatterns = "/ContactoServlet")
+public class ContactoServlet extends HttpServlet {
+
+    private ContactoDAO contactoDao;
+
+    public ContactoServlet() {
+        super();
+        contactoDao = new ContactoDAOImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      this.procesarSolicitud(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.procesarSolicitud(req,resp);
+    }
+
+    protected void procesarSolicitud(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        switch (request.getParameter("action")) {
+            case "list":
+                // this.list(request, response);
+                break;
+            case "create":
+                this.create(request, response);
+                break;
+            case "read":
+                // this.read(request, response);
+                break;
+            case "update":
+                //this.update(request, response);
+                break;
+            case "delete":
+                // this.delete(request, response);
+                break;
+            case "showRegister":
+                this.showRegister(request, response);
+                break;
+            case "index":
+                this.index(request, response);
+                break;
+            default:
+               this.index(request, response);
+                break;
+        }
+        
+    }
+        private void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, response);
+        }
+        
+        private void showRegister(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException{
+            RequestDispatcher dispacther = request.getRequestDispatcher("/view/create.jsp");
+            dispacther.forward(request, response);
+        
+        }
+        
+        private void create (HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+            String name = request.getParameter("nombre");
+            String apellido = request.getParameter("apellido");
+            String email = request.getParameter("email");
+            String descrip = request.getParameter("descripcion");
+            // Crear el objeto que se envia al BD
+            Contacto objContacto = new Contacto();
+            objContacto.setNombre(name);
+             objContacto.setApellido(apellido);
+              objContacto.setEmail(email);
+               objContacto.setDescripcion(descrip);
+            
+            contactoDao.insert(objContacto);
+            
+            // Redireccionar al "index"
+            
+            this.index(request, response);
+             
+            
+        }
+}
+
+
+
---
+++ src/main/java/com/pedro/model/Contacto.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pedro.model;
+
+
+public class Contacto {
+
+    //Atributos del modelo
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String descripcion;
+
+    public Contacto() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setDescripcion(String descrip) {
+        this.descripcion = descrip;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
